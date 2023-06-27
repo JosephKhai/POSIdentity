981350c674b9fd137dc71f907d08a16793f5fa3f
Pum Suan Khai SEKTAK <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-06-27 16:28:32 +1000

    Add project files.

---
+++ API/Controllers/PosShopController.cs
@@ -0,0 +1,25 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PosShopController : ControllerBase
+    {
+        private readonly IPosShopService _posShopService;
+
+        public PosShopController(IPosShopService posShopService)
+        {
+            _posShopService = posShopService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var posShop = await _posShopService.GetShopListAsync();
+            return Ok(posShop);
+        }
+    }
+}
---
+++ API/Services/PosShopService.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class PosShopService : IPosShopService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PosShopService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<POSShopDto>> GetShopListAsync()
+        {
+            var posShop = await (from shop in _dbContext.POSShops
+                              select new POSShopDto()
+                              {
+                                  Id = shop.Id,
+                                  Name = shop.Name,
+                                  Address = shop.Address,
+
+                              }).ToListAsync();
+
+            return posShop;
+        }
+    }
+}
